1561f614ef7535d0ba8c8dc582f4b65dec99849a
Phil Hawthorne <phil@myguestlist.com.au>
2016-07-17 15:55:35 +1000

    Re-enable discovery
    Let's try using discovery service again

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (userSettings) {
 }
 
 var fileServer = new nodeStatic.Server(webroot);
-var discovery = new SonosDiscovery({disableIpDiscovery: true, port: 8121});
+var discovery = new SonosDiscovery({port: 8121});
 var api = new SonosHttpAPI(discovery, settings);
 
 var requestHandler = function (req, res) {
